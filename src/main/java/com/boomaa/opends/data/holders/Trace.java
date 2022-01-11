b032f306aaf752394efde260a71a829ebaf8f74a
Boomaa23 <cmo93003@yahoo.com>
2022-01-10 20:55:16 -0800

    Update data holders to 2022 framework

--- src/main/java/com/boomaa/opends/data/holders/Trace.java
+++ src/main/java/com/boomaa/opends/data/holders/Trace.java
@@ -2,22 +2,22 @@ package com.boomaa.opends.data.holders;
 
 public enum Trace implements DataBase.Holder {
     ROBOTCODE(DataBase.create()
-            .addFlag(0x20, 2021, 2020)
+            .addFlag(0x20, 2022, 2021, 2020)
     ),
     ISROBORIO(DataBase.create()
-            .addFlag(0x10, 2021, 2020)
+            .addFlag(0x10, 2022, 2021, 2020)
     ),
     TESTMODE(DataBase.create()
-            .addFlag(0x08, 2021, 2020)
+            .addFlag(0x08, 2022, 2021, 2020)
     ),
     AUTOMODE(DataBase.create()
-            .addFlag(0x04, 2021, 2020)
+            .addFlag(0x04, 2022, 2021, 2020)
     ),
     TELEOPCODE(DataBase.create()
-            .addFlag(0x02, 2021, 2020)
+            .addFlag(0x02, 2022, 2021, 2020)
     ),
     DISABLED(DataBase.create()
-            .addFlag(0x01, 2021, 2020)
+            .addFlag(0x01, 2022, 2021, 2020)
     );
 
     public final DataBase flags;
