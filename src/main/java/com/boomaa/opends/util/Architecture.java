1f99934402e2bcd34afc3b8f9963b5f11da5ebde
Boomaa23 <cmo93003@yahoo.com>
2022-03-12 20:00:14 -0800

    Fix macos arch detection

--- src/main/java/com/boomaa/opends/util/Architecture.java
+++ src/main/java/com/boomaa/opends/util/Architecture.java
@@ -1,14 +1,22 @@
 package com.boomaa.opends.util;
 
 public enum Architecture {
-    AMD64,
-    AARCH64;
+    AMD64("amd64", "x86_64"),
+    AARCH64("aarch64, aarch64e");
+
+    private final String[] archNames;
+
+    Architecture(String... archNames) {
+        this.archNames = archNames;
+    }
 
     public static Architecture getCurrent() {
         String search = System.getProperty("os.arch").toLowerCase();
         for (Architecture arch : Architecture.values()) {
-            if (search.contains(arch.toString())) {
-                return arch;
+            for (String name : arch.archNames) {
+                if (search.contains(name)) {
+                    return arch;
+                }
             }
         }
         throw new NativeSystemError("Unsupported architecture " + search);
